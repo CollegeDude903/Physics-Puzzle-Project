beb19b2d9397c70886a2a9add8282959be1bea66
CollegeDude903 <blumpford@hotmail.com>
2015-10-16 22:08:57 -0700

    Finished Jetpack
    Player will now fall when out of jetpack fuel and must return to the
    ground to refuel

--- 350 Game/Assets/Scripts/FlyController.cs
+++ 350 Game/Assets/Scripts/FlyController.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class FlyController : MonoBehaviour {
 
 	public Rigidbody rb;
-	public float thrust;
-	public float maxVelocity;
-	public float explosionForce;
-	public float explosionRadius;
+	public float thrust = 300;
+	public float maxVelocity = 250;
+	public float explosionForce = 40;
+	public float explosionRadius = 20;
+	public float fuelTimer;
+	public float maxFuel = 10f;
 	float stopSpeed;
 	bool isGrounded = true;
+	bool needToRefuel = false;
 
 	void Start(){
 		rb = GetComponent<Rigidbody>();
+		fuelTimer = maxFuel;
 	}
 
 	void FixedUpdate () {
@@ -20,26 +24,45 @@ public class FlyController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Fly");
 		Vector3 movement = new Vector3 (0, moveVertical, 0);
 
+		//Add a little "Umph" to every first take-off
 		if (Input.GetAxis ("Fly") > 0 && isGrounded == true) {
 			rb.AddExplosionForce (explosionForce, transform.position, explosionRadius);
 		}
-		//Hover in mid-air when not flying up or down
-		if (Input.GetButtonUp ("Fly")){
-			stopSpeed = 1;
-		}
-		if (Input.GetButton ("Fly")) {
-			rb.velocity =  movement * thrust * Time.fixedDeltaTime;
-		} 
-		else if (isGrounded == false) {
-			stopSpeed *= .99f;
-			if (rb.velocity.y * movement.y > 0){
-				rb.velocity = movement * stopSpeed;
+
+
+		if (isGrounded == true) {
+			fuelTimer += Time.fixedDeltaTime;
+			if (fuelTimer >= maxFuel) fuelTimer = maxFuel;
+			needToRefuel = false;
+
+		} else {
+			fuelTimer -= Time.fixedDeltaTime;
+			if (fuelTimer <= 0) {
+				fuelTimer = 0;
+				needToRefuel = true;
+				rb.WakeUp ();
 			}
-			else {
-				rb.velocity = Vector3.zero;
-				rb.angularVelocity = Vector3.zero;
-				rb.Sleep();
-			}
+			if (needToRefuel == false) {
+				
+				//Hover in mid-air when not flying up or down
+				if (Input.GetButtonUp ("Fly")) {
+					stopSpeed = 1;
+				}
+				//Move up or down when holding down "Q" or "E"
+				if (Input.GetButton ("Fly")) {
+					rb.velocity = movement * thrust * Time.fixedDeltaTime;
+				} else if (isGrounded == false) {
+					
+					stopSpeed *= .99f;
+					if (rb.velocity.y * movement.y > 0) {
+						rb.velocity = movement * stopSpeed;
+					} else {
+						rb.velocity = Vector3.zero;
+						rb.angularVelocity = Vector3.zero;
+						rb.Sleep ();
+					}
+				}
+			} 
 		}
 		//print (transform.position.y);
 		//////////////////////////////////////////
@@ -48,7 +71,7 @@ public class FlyController : MonoBehaviour {
 		if (Mathf.Abs(rb.velocity.y) > maxVelocity) {;
 			rb.velocity = movement * maxVelocity * Time.fixedDeltaTime;
 		}
-		//print (movement * -thrust);
+		print (fuelTimer);
 		///////////////////////////////////////////////////////////
 	}
 	void OnCollisionEnter(Collision collision){
