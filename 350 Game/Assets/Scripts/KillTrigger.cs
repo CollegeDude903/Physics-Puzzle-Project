fa5b34b8d3097fa43b40270eb4ec9e478996f175
JasonSullender <jsonsullender@gmail.com>
2015-11-10 23:29:14 -0500

    Added Items
    Pause and reset buttons with a pause menu script
    outline for mainmenu and main menu code
    kill trigger for player fall off with kill trigger code

---
+++ 350 Game/Assets/Scripts/KillTrigger.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Written by Jason Sullender
+/// This class is written for the KillTrigger game object that is a box collider that is a trigger
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class KillTrigger : MonoBehaviour {
+	/// <summary>
+	/// When the player enters the KillTrigger game object trigger destroy player then load level
+	/// </summary>
+	/// <param name="col">Col.</param>
+	void OnTriggerEnter(Collider col)
+	{
+
+		
+
+			Destroy (col.gameObject);	
+			ReloadGame();
+		
+	}
+	
+	public void ReloadGame()
+	{			
+	
+		Application.LoadLevel ("Scene4(GitHub)");//loads scene
+
+	}
+}
